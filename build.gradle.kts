631d574595c2563582ddd5302f4b96529496d7ce
joserobjr <joserobjr@gamemods.com.br>
2020-06-03 19:45:05 -0300

    Setup maven publication

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,6 +20,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     kotlin("jvm") version "1.3.72"
     jacoco
+    `maven-publish`
 }
 
 java {
@@ -28,6 +29,7 @@ java {
 }
 
 val moduleName = "com.github.asyncmc.protocol.raknet"
+val isSnapshot = version.toString().endsWith("SNAPSHOT")
 
 repositories {
     jcenter()
@@ -111,6 +113,76 @@ tasks {
         reports {
             xml.isEnabled = true
             html.isEnabled = true
+        }
+    }
+
+    create<Jar>("sourceJar") {
+        archiveClassifier.set("sources")
+        from(sourceSets.main.get().allSource)
+    }
+
+    withType<Jar>().configureEach {
+        from(projectDir) {
+            include("LICENSE.txt")
+            include("NOTICE.md")
+        }
+    }
+}
+
+
+fun findProp(name: String) = findProperty(name)?.toString()?.takeIf { it.isNotBlank() }
+    ?: System.getenv(name.replace('.', '_').toUpperCase())?.takeIf { it.isNotBlank() }
+
+publishing {
+    repositories {
+        maven {
+            val prefix = if (isSnapshot) "asyncmc.repo.snapshot" else "asyncmc.repo.release"
+            url = uri(findProp("$prefix.url") ?: "$buildDir/repo")
+            when(findProp("$prefix.auth.type")) {
+                "password" -> credentials {
+                    username = findProp("$prefix.auth.username")
+                    password = findProp("$prefix.auth.password")
+                }
+                "aws" -> credentials(AwsCredentials::class.java) {
+                    accessKey = findProp("$prefix.auth.access_key")
+                    secretKey = findProp("$prefix.auth.secret_key")
+                    sessionToken = findProp("$prefix.auth.session_token")
+                }
+                "header" -> credentials(HttpHeaderCredentials::class.java) {
+                    name = findProp("$prefix.auth.header_name")
+                    value = findProp("$prefix.auth.header_value")
+                }
+            }
+        }
+    }
+
+    publications {
+        create<MavenPublication>("library") {
+            from(components["java"])
+            artifact(tasks["sourceJar"])
+            pom {
+                name.set("RakNet Protocol")
+                description.set("An async, non blocking, open source, copyleft RakNet library written in Kotlin")
+                url.set("https://github.com/AsyncMC/RakNet-Protocol")
+                licenses {
+                    license {
+                        name.set("GNU Affero General Public License, Version 3")
+                        url.set("https://www.gnu.org/licenses/agpl-3.0.html")
+                    }
+                }
+                developers {
+                    developer {
+                        id.set("joserobjr")
+                        name.set("José Roberto de Araújo Júnior")
+                        email.set("joserobjr@gamemods.com.br")
+                    }
+                }
+                scm {
+                    url.set("https://github.com/AsyncMC/RakNet-Protocol")
+                    connection.set("scm:git:https://github.com/AsyncMC/RakNet-Protocol.git")
+                    developerConnection.set("https://github.com/AsyncMC/RakNet-Protocol.git")
+                }
+            }
         }
     }
 }
